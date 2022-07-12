3847b646ee3daf0087b41c4dbace0d9702be5230
Dimitri Lopez <dimitrilopez01@gmail.com>
2022-07-12 14:07:24 -0400

    Fixing random number generator round 2

--- rand48.py
+++ rand48.py
@@ -33,59 +33,27 @@
 # The initializer function srand48() sets the high order 32-bits of Xi to
 # the  argument  seedval.  The low order 16-bits are set to the arbitrary
 # value 0x330E.
-import random
 
 
 # Generate a new 48bits
 def drand48():
-    return random.uniform(0, 1)
-    # global Xn
-    # try: Xn
-    # except: raise Exception("srand48() must be called before drand48() is used.")
-    # a = 0x5DEECE66D # from man pages
-    # c = 0xB         # from man pages
-    # m = 2**48       # The  parameter  m = 2^48, hence 48-bit integer arithmetic is performed.
+    global Xn
+    try: Xn
+    except: raise Exception("srand48() must be called before drand48() is used.")
+    a = 0x5DEECE66D # from man pages
+    c = 0xB         # from man pages
+    m = 2**48       # The  parameter  m = 2^48, hence 48-bit integer arithmetic is performed.
 
-    # # Xn+1 = (aXn + c) mod m, where n >= 0
-    # Xn1 = ((a * Xn) + c) % m
+    # Xn+1 = (aXn + c) mod m, where n >= 0
+    Xn1 = ((a * Xn) + c) % m
 
-    # Xn = Xn1
-    # return Xn1
+    Xn = Xn1
+    return Xn1 / 2**48
 
 # Seed the random number generator
 def srand48(seedval):
     # The initializer function srand48() sets the high order 32-bits of Xi to
     # the  argument  seedval.  The low order 16-bits are set to the arbitrary
     # value 0x330E.
-    random.seed(seedval)
-    return
-
-    # # bin(0x330E) = 0b11001100001110
-    # # bin(0x330E)[2:] = 11001100001110
-    # highorder = get_bits(seedval, 32)
-    # loworder = get_bits(0x330E, 16)
-    # print("highorder:", highorder)
-    # print("loworder:", loworder)
-    # print("seeded to:", highorder + loworder)
-    # print("len seeded to:", len(highorder + loworder))
-    # global Xn
-    # Xn = int("0b" + highorder + loworder)
-def get_bits(num, bit_amount):
-    bits = bin(num)[2:]
-    leading_zeros = bit_amount - len(bits)
-    return ("0" * leading_zeros) + bits
-
-
-# srand48(2)
-# print("Xn:", Xn)
-# print("bin Xn:", bin(Xn))
-# print("len bin Xn:", len(bin(Xn)))
-# print("drand:", drand48())
-
-# import struct
-
-# f = int('01000001101011000111101011100001', 2)
-# print struct.unpack('f', struct.pack('I', f))[0]
-
-# f = int('11000001101011000111101011100001', 2)
-# print struct.unpack('f', struct.pack('I', f))[0]
+    global Xn
+    Xn = (seedval << 16) + 0x330E
