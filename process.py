f83a71b3c606725485a9a4e839eba326b7b5b12d
Dimitri Lopez <dimitrilopez01@gmail.com>
2022-07-15 11:58:24 -0400

    Forgot to add process changes

--- process.py
+++ process.py
@@ -12,7 +12,8 @@ class Process():
         self.tau = math.ceil(1 / l)      # Initial tau value
         self.finish = None               # Finish time for the process
         self.curr_burst = burst_times[0] # Time left on burst that the process is currently on
-        self.curr_io = io_times[0]       # I/O end time for current burst (time+io_time)
+        if len(io_times) > 0:
+            self.curr_io = io_times[0]   # I/O end time for current burst (time+io_time)
 
         self.remaining_bursts = self.cpu_bursts
 
@@ -33,6 +34,10 @@ class Process():
 
     def run_burst(self):
         return self.burst_times.pop(0)
+    def run_io(self):
+        return self.io_times.pop(0)
+    def rbursts(self):
+        return len(self.burst_times)
 
     def __str__(self):
         string = ""
@@ -43,6 +48,13 @@ class Process():
         return string
     def __repr__(self):
         return self.__str__
+    def sprint(self):
+        return f"Process {self.pid} (tau {self.tau}ms)"
+    def calc_new_tau(self, curr_time, alpha, time):
+        old_tau = self.tau
+        self.tau = math.ceil(alpha * time + (1 - alpha) * old_tau)
+        return[old_tau, self.tau]
+
 
     # def reset_process(self):
     #     self.finish = None # prcess hasn't finished
@@ -55,17 +67,17 @@ class Event():
     PREMPTION = "PREMPTION"
     CS_START = "CS_STAT"
     CS_END = "CS_END"
-    CPU_BURST = "CPU_BURST"
+    CPU_BURST_END = "CPU_BURST"
 
-    def __init__(self, process, time, etype):
-        assert(etype in EVENT_TYPE)
+    def __init__(self, process, start, time, etype):
         self.process = process
+        self.start = start
         self.time = time
         self.etype = etype
     def __lt__(self, other):
         return self.time < other.time
     def __str__(self):
-        string = f"EVENT: time: {self.time} type: {self.etype}"
+        string = f"EVENT: time: {self.start + self.time} start: {self.start} type: {self.etype}"
         return string
     def __repr__(self):
         return self.__str__()
